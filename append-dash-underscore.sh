17962f1cb66563b3b57ab6c3312dfb6d3bc50b38
Alard <alard@example.com>
2011-11-13 22:38:25 +0100

    Add fix script for missing - and _.

---
+++ append-dash-underscore.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# Downloads files ending with _ and - from the given prefix.
+#
+# Usage:  ./append-dash-underscore.sh {PREFIX}
+# where PREFIX is the three-letter prefix for the range.
+#
+
+# this script needs wget-warc, which you can find on the ArchiveTeam wiki.
+
+WGET_WARC=./wget-warc
+if [ ! -x $WGET_WARC ]
+then
+  echo "./wget-warc not found."
+  exit 3
+fi
+
+VERSION="20111113.02"
+
+USER_AGENT="Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27"
+
+prefix=$1
+
+prefixdir="data/$1"
+
+mkdir -p "${prefixdir}"
+touch "${prefixdir}/.incomplete"
+
+for c in - _
+do
+  echo "http://f.anyhub.net/${prefix}${c}"
+done > "${prefixdir}/urls-${prefix}-dash.txt"
+
+date=$( date +'%Y%m%d' )
+
+result=8
+tries=1
+while [ $result -eq 8 ]
+do
+  echo "  Downloading prefix: ${prefix}  try: ${tries}"
+  $WGET_WARC -U "${USER_AGENT}" -e "robots=off" \
+    -nv -o "${prefixdir}/wget-${prefix}-d${tries}.log" \
+    -O /dev/null \
+    --max-redirect=0 \
+    --warc-file="${prefixdir}/anyhub.net-${prefix}_-${date}-d${tries}" \
+    --warc-max-size=inf \
+    --warc-header="operator: Archive Team" \
+    --warc-header="anyhub-range-prefix: ${prefix}" \
+    --input-file="${prefixdir}/urls-${prefix}-d${tries}.txt"
+  result=$?
+  if [ $result -eq 8 ]
+  then
+    next_tries=$(( tries + 1 ))
+    grep -B 1 'ERROR 50' "${prefixdir}/urls-${prefix}-d${tries}.txt" \
+      | grep -oE "http://[^:]+" \
+      > "${prefixdir}/urls-${prefix}-d${next_tries}.txt"
+    if [ -s "${prefixdir}/urls-${prefix}-d${next_tries}.txt" ]
+    then
+      tries=$next_tries
+      result=9
+    else
+      result=0
+    fi
+  fi
+done
+
+echo -n "  Prefix ${prefix} done: "
+./du-helper.sh -hs "$prefixdir/"
+
+rm "${prefixdir}/.incomplete"
+
+exit 0
+
---
+++ fix-dld.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# Fix script errors in early downloads.
+#
+# This script will look in your data/ directory for downloads and
+# fix the following (if necessary):
+#
+#  * Add - and _ to downloads that don't have it.
+#
+# Note: this script will NOT fix any user that's still being
+# downloaded, that is, anything that has an .incomplete file.
+# This means that you can run this script while a normal
+# client is downloading, but you can't use this script to fix
+# interrupted downloads.
+#
+# Usage:   fix-dld.sh ${YOURALIAS}
+#
+
+youralias="$1"
+
+if [[ ! $youralias =~ ^[-A-Za-z0-9_]+$ ]]
+then
+  echo "Usage:  $0 {nickname}"
+  echo "Run with a nickname with only A-Z, a-z, 0-9, - and _"
+  exit 4
+fi
+
+initial_stop_mtime='0'
+if [ -f STOP ]
+then
+  initial_stop_mtime=$( stat -c '%Y' STOP )
+fi
+
+for d in data/*
+do
+  prefix=$( basename "$d" )
+  need_fix=0
+
+  if [ -f "${d}/.incomplete" ]
+  then
+    echo "${prefix} is still incomplete, not fixing."
+    continue
+  fi
+
+  # FIX 1: check for download of - and _
+  if ! grep -q "${prefix}_" "${d}/wget"*".log"
+  then
+    echo "${prefix} is missing - and _, needs to be fixed."
+    touch "${d}/.incomplete"
+    if ./append-dash-underscore.sh "${prefix}"
+    then
+      need_fix=1
+    fi
+  fi
+
+  # fix, if necessary
+  if [[ $need_fix -eq 1 ]]
+  then
+    if ! ./dld-single.sh "$youralias" "${prefix}"
+    then
+      exit 6
+    fi
+  fi
+
+  if [ -f STOP ] && [[ $( stat -c '%Y' STOP ) -gt $initial_stop_mtime ]]
+  then
+    exit
+  fi
+done
+
